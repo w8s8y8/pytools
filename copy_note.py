dbc0a6d4179ced9bbe78cafe90b93bda75aa41b1
w8s8y8 <w8s8y8@126.com>
2018-07-09 16:40:30 +0800

    Fisrt

---
+++ copy_note.py
@@ -0,0 +1,24 @@
+﻿import os
+import shutil
+import time
+
+
+rootdir = 'F:\\WSHY\\札记'
+
+desdir = '札记'
+
+if __name__ == '__main__':
+    print('---==== 札记自动备份工具 ====---\n')
+    
+    desdir = desdir + time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    
+    for file_name in os.listdir(rootdir):
+        sub_file_name = os.path.join(rootdir, file_name)
+        if os.path.isdir(sub_file_name):
+            if len(os.listdir(sub_file_name)) > 0:
+                shutil.copytree(sub_file_name, os.path.join(desdir, file_name))
+                shutil.rmtree(sub_file_name)
+            else:
+                os.rmdir(sub_file_name)
+    
+    
